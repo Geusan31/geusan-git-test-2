1aeddefa064c8c560afaf347ac9beaf1d9614b49
Geusan Edurais <edurais.ariadaffa@gmail.com>
2022-03-09 13:27:02 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic veritatis neque modi delectus voluptates, facilis laborum! Laboriosam ad pariatur at deleniti, vitae, debitis dignissimos rem architecto vel, quaerat amet dolore.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
